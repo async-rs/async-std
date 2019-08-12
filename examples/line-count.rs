fdf0e3ad0f5f75442f5dc1b7845c8333e14dbe18
Stjepan Glavina <stjepang@gmail.com>
2019-08-12 12:44:10 +0200

    Add the line-count example

---
+++ examples/line-count.rs
@@ -0,0 +1,29 @@
+//! Counts the number of lines in a file given as an argument.
+
+#![feature(async_await)]
+
+use std::env::args;
+
+use async_std::fs::File;
+use async_std::io::{self, BufReader};
+use async_std::prelude::*;
+use async_std::task;
+
+fn main() -> io::Result<()> {
+    let path = args().nth(1).expect("missing path argument");
+
+    task::block_on(async {
+        let file = BufReader::new(File::open(&path).await?);
+
+        let mut lines = file.lines();
+        let mut count = 0u64;
+
+        while let Some(line) = lines.next().await {
+            line?;
+            count += 1;
+        }
+
+        println!("The file contains {} lines.", count);
+        Ok(())
+    })
+}
