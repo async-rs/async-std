e4079640d77c6788b98b44aa449832e61386644c
Florian Gilcher <florian.gilcher@asquera.de>
2019-08-16 20:34:34 +0200

    Let's try it this way...

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -1,9 +1,9 @@
 //! OS-specific extensions.
 
-#[cfg(any(unix, feature = "docs"))]
 #[cfg_attr(feature = "docs", doc(cfg(unix)))]
+#[cfg(any(unix, feature = "docs"))]
 pub mod unix;
 
-#[cfg(any(windows, feature = "docs"))]
 #[cfg_attr(feature = "docs", doc(cfg(windows)))]
+#[cfg(any(windows, feature = "docs"))]
 pub mod windows;
