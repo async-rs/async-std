ba5586b0505b594db66b950e4b2268b16b353ff3
Stjepan Glavina <stjepang@gmail.com>
2019-08-29 11:43:41 +0200

    Simplify ReadDir and DirEntry

--- src/fs/read_dir.rs
+++ src/fs/read_dir.rs
@@ -1,7 +1,6 @@
 use std::fs;
 use std::path::Path;
 use std::pin::Pin;
-use std::sync::Mutex;
 
 use super::DirEntry;
 use crate::future::Future;
@@ -64,34 +63,21 @@ pub async fn read_dir<P: AsRef<Path>>(path: P) -> io::Result<ReadDir> {
 /// [`DirEntry`]: struct.DirEntry.html
 /// [`std::fs::ReadDir`]: https://doc.rust-lang.org/std/fs/struct.ReadDir.html
 #[derive(Debug)]
-pub struct ReadDir(Mutex<State>);
+pub struct ReadDir(State);
 
 /// The state of an asynchronous `ReadDir`.
 ///
 /// The `ReadDir` can be either idle or busy performing an asynchronous operation.
 #[derive(Debug)]
 enum State {
-    Idle(Option<Inner>),
-    Busy(blocking::JoinHandle<State>),
-}
-
-/// Inner representation of an asynchronous `DirEntry`.
-#[derive(Debug)]
-struct Inner {
-    /// The blocking handle.
-    read_dir: fs::ReadDir,
-
-    /// The next item in the stream.
-    item: Option<io::Result<DirEntry>>,
+    Idle(Option<fs::ReadDir>),
+    Busy(blocking::JoinHandle<(fs::ReadDir, Option<io::Result<fs::DirEntry>>)>),
 }
 
 impl ReadDir {
     /// Creates an asynchronous `ReadDir` from a synchronous handle.
     pub(crate) fn new(inner: fs::ReadDir) -> ReadDir {
-        ReadDir(Mutex::new(State::Idle(Some(Inner {
-            read_dir: inner,
-            item: None,
-        }))))
+        ReadDir(State::Idle(Some(inner)))
     }
 }
 
@@ -99,36 +85,23 @@ impl futures::Stream for ReadDir {
     type Item = io::Result<DirEntry>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let state = &mut *self.0.lock().unwrap();
-
         loop {
-            match state {
+            match &mut self.0 {
                 State::Idle(opt) => {
-                    let inner = match opt.as_mut() {
-                        None => return Poll::Ready(None),
-                        Some(inner) => inner,
-                    };
+                    let mut inner = opt.take().unwrap();
 
-                    // Check if the operation has completed.
-                    if let Some(res) = inner.item.take() {
-                        return Poll::Ready(Some(res));
-                    } else {
-                        let mut inner = opt.take().unwrap();
-
-                        // Start the operation asynchronously.
-                        *state = State::Busy(blocking::spawn(async move {
-                            match inner.read_dir.next() {
-                                None => State::Idle(None),
-                                Some(res) => {
-                                    inner.item = Some(res.map(DirEntry::new));
-                                    State::Idle(Some(inner))
-                                }
-                            }
-                        }));
-                    }
+                    // Start the operation asynchronously.
+                    self.0 = State::Busy(blocking::spawn(async move {
+                        let next = inner.next();
+                        (inner, next)
+                    }));
                 }
                 // Poll the asynchronous operation the file is currently blocked on.
-                State::Busy(task) => *state = futures::ready!(Pin::new(task).poll(cx)),
+                State::Busy(task) => {
+                    let (inner, opt) = futures::ready!(Pin::new(task).poll(cx));
+                    self.0 = State::Idle(Some(inner));
+                    return Poll::Ready(opt.map(|res| res.map(DirEntry::new)));
+                }
             }
         }
     }
