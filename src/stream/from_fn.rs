4ad4209cd5fd2e3600281eab6a043f937a8aef48
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-08-30 20:04:36 +0200

    use taiki-e version, and atomic example
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/stream/from_fn.rs
+++ src/stream/from_fn.rs
@@ -1,81 +1,100 @@
+use futures::stream::Stream;
+use pin_utils::{unsafe_pinned, unsafe_unpinned};
+
 use std::fmt;
 use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
-use futures::ready;
-use futures::stream::Stream;
-
-/// Creates a new stream where each iteration calls the provided closure.
+/// Creates a new stream where each iteration calls the provided closure
+/// `F: FnMut() -> Option<T>`.
 ///
-/// This allows creating a custom iterator with any behavior
+/// This allows creating a custom stream with any behavior
 /// without using the more verbose syntax of creating a dedicated type
-/// and implementing the `Iterator` trait for it.
+/// and implementing the `Stream` trait for it.
+///
+/// Note that the `FromFn` stream doesn’t make assumptions about the behavior of the closure,
+/// and therefore conservatively does not implement [`FusedStream`](futures_core::stream::FusedStream).
+///
+/// The closure can use captures and its environment to track state across iterations. Depending on
+/// how the stream is used, this may require specifying the `move` keyword on the closure.
 ///
 /// # Examples
-/// 
+///
 /// ```
-/// # fn main() { async_std::task::block_on(async {
-/// #
-/// let mut count = 0;
-/// let counter = async_std::stream::from_fn(|| async move {
+/// # futures::executor::block_on(async {
+/// use async_std::{future, stream};
+/// use std::sync::atomic::{AtomicUsize, Ordering};
+///
+/// static COUNT: AtomicUsize = AtomicUsize::new(0);
+/// let stream = stream::from_fn(|| {
 ///     // Increment our count. This is why we started at zero.
-///     count += 1;
+///
+///     let count = COUNT.fetch_add(1, Ordering::SeqCst);
 ///
 ///     // Check to see if we've finished counting or not.
 ///     if count < 6 {
-///         Some(count)
+///         future::ready(Some(count))
 ///     } else {
-///         None
+///         future::ready(None)
 ///     }
 /// });
-/// assert_eq!(counter.collect::<Vec<_>>().await, &[1, 2, 3, 4, 5]);
-/// #
-/// # }) }
+/// assert_eq!(stream.collect::<Vec<_>>().await, &[1, 2, 3, 4, 5]);
+/// # });
 /// ```
-#[inline]
-pub fn from_fn<T, F>(f: F) -> FromFn<F, T>
-    where F: FnMut() -> Box<dyn Future<Output = Option<T>>>,
-          F: Unpin
+pub fn from_fn<F, Fut, Item>(f: F) -> FromFn<F, Fut>
+where
+    F: FnMut() -> Fut,
+    Fut: Future<Output = Option<Item>>,
 {
-    FromFn{
-        closure: f,
-        fut: None,
+    FromFn { f, fut: None }
+}
+
+/// Stream for the [`from_fn`] function.
+#[must_use = "streams do nothing unless polled"]
+pub struct FromFn<F, Fut> {
+    f: F,
+    fut: Option<Fut>,
+}
+
+impl<F, Fut: Unpin> Unpin for FromFn<F, Fut> {}
+
+impl<F, Fut> fmt::Debug for FromFn<F, Fut>
+where
+    Fut: fmt::Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("FromFn").field("fut", &self.fut).finish()
     }
 }
 
-/// A stream where each iteration calls the provided closure.
-///
-/// This `struct` is created by the [`stream::from_fn`] function.
-/// See its documentation for more.
-///
-/// [`stream::from_fn`]: fn.from_fn.html
-pub struct FromFn<F, T> {
-    closure: F,
-    fut: Option<Box<dyn Future<Output = Option<T>>>>,
+impl<F, Fut> FromFn<F, Fut> {
+    unsafe_unpinned!(f: F);
+    unsafe_pinned!(fut: Option<Fut>);
 }
 
-impl<T, F: Unpin> Stream for FromFn<F, T>
-    where F: FnMut() -> Box<dyn Future<Output = Option<T>>>
+impl<F, Fut, Item> Stream for FromFn<F, Fut>
+where
+    F: FnMut() -> Fut,
+    Fut: Future<Output = Option<Item>>,
 {
-    type Item = T;
+    type Item = Item;
 
     #[inline]
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        if let None = self.fut {
-            self.fut = Some((self.closure)());
+        if self.fut.is_none() {
+            let fut = (self.as_mut().f())();
+            self.as_mut().fut().set(Some(fut));
         }
-        
-        let pinned = unsafe { Pin::new_unchecked(&mut self.fut.as_mut().unwrap()) };
-        let out = ready!(pinned.poll(cx));
 
-        self.fut = None;
-        Poll::Ready(out)
-    }
-}
-
-impl<F, T> fmt::Debug for FromFn<F, T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("FromFn").finish()
+        self.as_mut()
+            .fut()
+            .as_pin_mut()
+            .unwrap()
+            .poll(cx)
+            .map(|item| {
+                self.as_mut().fut().set(None);
+                item
+            })
     }
 }
