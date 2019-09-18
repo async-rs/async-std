b670600555087d013be35a872d1b019bdb5b029c
Yoshua Wuyts <yoshuawuyts+github@gmail.com>
2019-09-18 12:42:46 +0200

    Update src/stream/into_stream.rs
    Co-Authored-By: Stjepan Glavina <stjepang@gmail.com>

--- src/stream/into_stream.rs
+++ src/stream/into_stream.rs
@@ -14,7 +14,7 @@ use futures_core::stream::Stream;
 ///
 /// [`FromStream`]: trait.FromStream.html
 #[cfg_attr(feature = "docs", doc(cfg(unstable)))]
-#[cfg(feature = "unstable")]
+#[cfg(any(feature = "unstable", feature = "docs"))]
 pub trait IntoStream {
     /// The type of the elements being iterated over.
     type Item;
