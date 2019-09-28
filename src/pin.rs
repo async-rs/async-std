e1ca5a4da9b1fe764e84f239ab4985dd2578e397
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-09-28 18:31:43 +0200

    expose pin-project on unstable
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/pin.rs
+++ src/pin.rs
@@ -4,3 +4,18 @@
 
 #[doc(inline)]
 pub use std::pin::Pin;
+
+#[doc(inline)]
+pub use pin_project::pin_project;
+
+#[doc(inline)]
+pub use pin_project::pinned_drop;
+
+#[doc(inline)]
+pub use pin_project::project;
+
+#[doc(inline)]
+pub use pin_project::project_ref;
+
+#[doc(inline)]
+pub use pin_project::UnsafeUnpin;
