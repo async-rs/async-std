d7e8b2d501af95caeba1a46d0b56d5b62f49dcd8
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-09-29 04:14:29 +0200

    example
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/future/into_future.rs
+++ src/future/into_future.rs
@@ -1,6 +1,35 @@
 use crate::future::Future;
 
 /// Convert a type into a `Future`.
+///
+/// # Examples
+///
+/// ```
+/// use async_std::future::{Future, IntoFuture};
+/// use async_std::io;
+/// use async_std::pin::Pin;
+///
+/// struct Client;
+///
+/// impl Client {
+///     pub async fn send(self) -> io::Result<()> {
+///         // Send a request
+///         Ok(())
+///     }
+/// }
+///
+/// impl IntoFuture for Client {
+///     type Output = io::Result<()>;
+///
+///     type Future = Pin<Box<dyn Future<Output = Self::Output>>>;
+///
+///     fn into_future(self) -> Self::Future {
+///         Box::pin(async {
+///             self.send().await
+///         })
+///     }
+/// }
+/// ```
 pub trait IntoFuture {
     /// The type of value produced on completion.
     type Output;
