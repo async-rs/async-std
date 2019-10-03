b2174576b27b4995cea5cc40754933b33370d990
Sunjay Varma <varma.sunjay@gmail.com>
2019-10-02 21:03:54 -0400

    Added FromStream + Extend for LinkedList

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -9,6 +9,7 @@ pub mod hash_set;
 pub mod btree_map;
 pub mod btree_set;
 pub mod binary_heap;
+pub mod linked_list;
 
 pub use vec_deque::VecDeque;
 pub use hash_map::HashMap;
@@ -16,3 +17,4 @@ pub use hash_set::HashSet;
 pub use btree_map::BTreeMap;
 pub use btree_set::BTreeSet;
 pub use binary_heap::BinaryHeap;
+pub use linked_list::LinkedList;
