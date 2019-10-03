8e127980389719aa9c2a16c832bce7eb579ba084
Sunjay Varma <varma.sunjay@gmail.com>
2019-10-02 21:11:50 -0400

    impl FromStream for Cow<[T]>

--- src/vec/from_stream.rs
+++ src/vec/from_stream.rs
@@ -1,4 +1,5 @@
 use std::pin::Pin;
+use std::borrow::Cow;
 
 use crate::stream::{Extend, FromStream, IntoStream};
 
@@ -18,6 +19,24 @@ impl<T> FromStream<T> for Vec<T> {
             let mut out = vec![];
             out.stream_extend(stream).await;
             out
+        })
+    }
+}
+
+impl<'b, T: Clone> FromStream<T> for Cow<'b, [T]> {
+    #[inline]
+    fn from_stream<'a, S: IntoStream<Item = T>>(
+        stream: S,
+    ) -> Pin<Box<dyn core::future::Future<Output = Self> + 'a>>
+    where
+        <S as IntoStream>::IntoStream: 'a,
+    {
+        let stream = stream.into_stream();
+
+        Box::pin(async move {
+            pin_utils::pin_mut!(stream);
+
+            Cow::Owned(FromStream::from_stream(stream).await)
         })
     }
 }
