ba29f4dc6b1b24394f04677376f82f344c45f8c2
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-10-15 16:23:15 +0200

    stabilize future::timeout
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/future/mod.rs
+++ src/future/mod.rs
@@ -56,17 +56,17 @@ use cfg_if::cfg_if;
 pub use pending::pending;
 pub use poll_fn::poll_fn;
 pub use ready::ready;
+pub use timeout::{timeout, TimeoutError};
 
 mod pending;
 mod poll_fn;
 mod ready;
+mod timeout;
 
 cfg_if! {
     if #[cfg(any(feature = "unstable", feature = "docs"))] {
         mod into_future;
-        mod timeout;
 
         pub use into_future::IntoFuture;
-        pub use timeout::{timeout, TimeoutError};
     }
 }
