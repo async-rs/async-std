40c4e1a29d7946faa5052389517d1f01b0f2d7d9
k-nasa <htilcs1115@gmail.com>
2019-10-30 10:33:59 +0900

    feat: Add stream::from_iter

---
+++ src/stream/from_iter.rs
@@ -0,0 +1,44 @@
+use std::pin::Pin;
+
+use pin_project_lite::pin_project;
+
+use crate::stream::Stream;
+use crate::task::{Context, Poll};
+
+pin_project! {
+    #[derive(Debug)]
+    pub struct FromIter<I> {
+        iter: I,
+    }
+}
+
+/// # Examples
+///```
+/// # async_std::task::block_on(async {
+/// #
+/// use async_std::prelude::*;
+/// use async_std::stream;
+///
+/// let mut s = stream::from_iter(vec![0, 1, 2, 3]);
+///
+/// assert_eq!(s.next().await, Some(0));
+/// assert_eq!(s.next().await, Some(1));
+/// assert_eq!(s.next().await, Some(2));
+/// assert_eq!(s.next().await, Some(3));
+/// assert_eq!(s.next().await, None);
+/// #
+/// # })
+///````
+pub fn from_iter<I: IntoIterator>(iter: I) -> FromIter<<I as std::iter::IntoIterator>::IntoIter> {
+    FromIter {
+        iter: iter.into_iter(),
+    }
+}
+
+impl<I: Iterator> Stream for FromIter<I> {
+    type Item = I::Item;
+
+    fn poll_next(mut self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        Poll::Ready(self.iter.next())
+    }
+}
