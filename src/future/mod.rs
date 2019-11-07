764f200a8d901644317d23bd00ee05966698bd42
Stjepan Glavina <stjepang@gmail.com>
2019-11-08 00:42:29 +0100

    Move future::timeout() behind cfg_default

--- src/future/mod.rs
+++ src/future/mod.rs
@@ -53,13 +53,16 @@ pub use future::Future;
 pub use pending::pending;
 pub use poll_fn::poll_fn;
 pub use ready::ready;
-pub use timeout::{timeout, TimeoutError};
 
 pub(crate) mod future;
 mod pending;
 mod poll_fn;
 mod ready;
-mod timeout;
+
+cfg_default! {
+    pub use timeout::{timeout, TimeoutError};
+    mod timeout;
+}
 
 cfg_unstable! {
     pub use into_future::IntoFuture;
