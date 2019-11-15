d0ef48c75354445b7f6e89e0c34d587859140158
Felipe Sere <felipesere@gmail.com>
2019-11-15 22:07:25 +0100

    Sketch out nth_back

--- src/stream/double_ended/mod.rs
+++ src/stream/double_ended/mod.rs
@@ -1,8 +1,13 @@
+mod nth_back;
+
+use nth_back::NthBackFuture;
+
 extension_trait! {
     use crate::stream::Stream;
 
     use std::pin::Pin;
     use std::task::{Context, Poll};
+
 
     #[doc = r#"
     Something fancy
@@ -17,6 +22,36 @@ extension_trait! {
         Something else
     "#]
     pub trait DoubleEndedStreamExt: crate::stream::DoubleEndedStream {
+
+        #[doc = r#"
+            Returns the nth element from the back of the stream.
+
+            # Examples
+
+            Basic usage:
+
+            ```
+            # fn main() { async_std::task::block_on(async {
+            #
+            use async_std::stream::double_ended::DoubleEndedStreamExt;
+            use async_std::stream;
+
+            let mut s = stream::from_iter(vec![1u8, 2, 3, 4, 5]);
+
+            let second = s.nth_back(1).await;
+            assert_eq!(second, Some(4));
+            #
+            # }) }
+            ```
+        "#]
+        fn nth_back(
+            &mut self,
+            n: usize,
+        ) -> impl Future<Output = Option<Self::Item>> + '_ [NthBackFuture<'_, Self>]
+        where
+            Self: Unpin + Sized,
+        {
+            NthBackFuture::new(self, n)
+        }
     }
 }
-
