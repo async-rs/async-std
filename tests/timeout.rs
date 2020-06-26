8f17e9275ba3f6470f5dad8945d97870602a79f1
dignifiedquire <me@dignifiedquire.com>
2020-06-26 12:48:23 +0200

    test: try to stabilize CI

--- tests/timeout.rs
+++ tests/timeout.rs
@@ -10,9 +10,9 @@ wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
 fn timeout_future_many() {
     task::block_on(async {
-        let futures = (0..100)
+        let futures = (0..10)
             .map(|i| {
-                timeout(Duration::from_millis(i * 20), async move {
+                timeout(Duration::from_millis(i * 50), async move {
                     task::sleep(Duration::from_millis(i)).await;
                     Ok::<(), async_std::future::TimeoutError>(())
                 })
