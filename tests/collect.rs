c6836ec4750a1679d4d5c23f8ae178fc5ca2e78e
dignifiedquire <me@dignifiedquire.com>
2020-06-27 14:45:59 +0200

    fixup test

--- tests/collect.rs
+++ tests/collect.rs
@@ -1,18 +1,20 @@
-use async_std::prelude::*;
-use async_std::io;
-use async_std::task;
-
+#[cfg(feature = "unstable")]
 #[test]
-fn test_send() -> io::Result<()> {
+fn test_send() -> async_std::io::Result<()> {
+    use async_std::prelude::*;
+    use async_std::{stream, task};
+
     task::block_on(async {
         fn test_send_trait<T: Send>(_: &T) {}
 
-        let stream = futures::stream::pending::<()>();
+        let stream = stream::repeat(1u8).take(10);
         test_send_trait(&stream);
 
         let fut = stream.collect::<Vec<_>>();
 
         // This line triggers a compilation error
         test_send_trait(&fut);
+
+        Ok(())
     })
 }
