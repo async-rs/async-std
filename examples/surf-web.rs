f0c8a8b4e9a2e7d130a494f4fb5c00d82c2c5c97
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-10-18 09:12:36 +0200

    update surf
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- examples/surf-web.rs
+++ examples/surf-web.rs
@@ -1,15 +1,16 @@
 use async_std::task;
 
-fn main() -> Result<(), surf::Exception> {
+fn main() -> Result<(), surf::Error> {
     task::block_on(async {
         let url = "https://www.rust-lang.org";
-        let mut response = surf::get(url).await?;
+        let mut response = surf::get(url).send().await?;
         let body = response.body_string().await?;
 
         dbg!(url);
         dbg!(response.status());
         dbg!(response.version());
-        dbg!(response.headers());
+        dbg!(response.header_names());
+        dbg!(response.header_values());
         dbg!(body.len());
 
         Ok(())
