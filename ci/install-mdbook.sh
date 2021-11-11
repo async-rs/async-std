40741ff2e42fb9032bf0386aa26675931404a6d4
Dima Pristupa <dima.pristupa@pm.me>
2021-11-11 13:18:59 +0200

    💚 Use the latest build of mdBook
    * No explicit tag means the latest

--- ci/install-mdbook.sh
+++ ci/install-mdbook.sh
@@ -4,16 +4,8 @@ set -euxo pipefail
 # https://github.com/rust-embedded/book/blob/master/ci/install.sh
 
 main() {
-    # Note - this will only accept releases tagged with v0.3.x
-    local tag=$(git ls-remote --tags --refs --exit-code \
-                    https://github.com/rust-lang-nursery/mdbook \
-                        | cut -d/ -f3 \
-                        | grep -E '^v0\.3\.[0-9]+$' \
-                        | sort --version-sort \
-                        | tail -n1)
-
     curl -LSfs https://japaric.github.io/trust/install.sh | \
-        sh -s -- --git rust-lang-nursery/mdbook --tag $tag
+        sh -s -- --git rust-lang-nursery/mdbookk
 }
 
 main
