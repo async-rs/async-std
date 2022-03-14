cca0f3e3212e62427cb972a21bf8a46478166315
Nicholas Nethercote <n.nethercote@gmail.com>
2022-03-15 09:53:28 +1100

    Use the default `recursion_limit`.
    Now that `extension_trait!` is gone, an increased limit isn't necessary.

--- src/lib.rs
+++ src/lib.rs
@@ -282,7 +282,6 @@
 #![doc(test(attr(deny(rust_2018_idioms, warnings))))]
 #![doc(test(attr(allow(unused_extern_crates, unused_variables))))]
 #![doc(html_logo_url = "https://async.rs/images/logo--hero.svg")]
-#![recursion_limit = "2048"]
 
 extern crate alloc;
 
