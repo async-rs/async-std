5e74d1b88da2233c6f4d443804eb27545c74164b
Josh Triplett <josh@joshtriplett.org>
2025-03-01 21:20:14 +0000

    Remove `deny(warnings)` which is causing CI to fail

--- src/lib.rs
+++ src/lib.rs
@@ -281,7 +281,7 @@
 #![cfg_attr(feature = "docs", feature(doc_cfg))]
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
 #![allow(clippy::mutex_atomic, clippy::module_inception)]
-#![doc(test(attr(deny(rust_2018_idioms, warnings))))]
+#![doc(test(attr(deny(rust_2018_idioms))))]
 #![doc(test(attr(allow(unused_extern_crates, unused_variables))))]
 #![doc(html_logo_url = "https://async.rs/images/logo--hero.svg")]
 
